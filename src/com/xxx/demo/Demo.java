b39d813fd2fa5e7d467f465c0614cf94fe9104d4
1024LTCoder <70096637+1024LTCoder@users.noreply.github.com>
2020-08-24 15:52:08 +0800

    Update Demo.java

--- src/com/xxx/demo/Demo.java
+++ src/com/xxx/demo/Demo.java
@@ -6,6 +6,6 @@ public class Demo {
         System.out.println("你好啊，兄弟萌!");
         System.out.println("你好啊，兄弟萌!1");
         System.out.println("你好啊，兄弟萌!2");
-        System.out.println("你好啊，兄弟萌!2");
+        System.out.println("你好啊，兄弟萌!3");
     }
 }
