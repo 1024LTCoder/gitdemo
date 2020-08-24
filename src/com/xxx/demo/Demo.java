8f996a30b08690a9164b7467fbbe27f643aaece1
1024LTCoder <litao2_1024@163.com>
2020-08-24 15:48:49 +0800

    加了一条代码

--- src/com/xxx/demo/Demo.java
+++ src/com/xxx/demo/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("你好啊，兄弟萌!");
         System.out.println("你好啊，兄弟萌!1");
+        System.out.println("你好啊，兄弟萌!2");
     }
 }
